b0d385b365857e9d1f818d7701d2521b96fd9195
theeunseojo <ces80372@naver.com>
2023-08-10 02:22:26 +0900

    2023.08.10 simplepace 경로 추가

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Routes,Route} from 'react-router-dom';
 import Login from './login/Login';
 import Sign from './sign/sign';
-import Main from './main/Main';
+import SimplePage from './simplepage/SimplePage';
 
 function App() { 
     return(
         <div className="App">
             <Routes>
-                <Route path ="/" element = { <Main/>} />
+                <Route path ="/simplepage" element = { <SimplePage/>} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Sign />} />
             </Routes>
