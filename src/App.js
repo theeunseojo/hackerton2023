fd12064d28d390eaaba2a96abc916d55aee366a1
theeunseojo <ces80372@naver.com>
2023-08-09 20:12:20 +0900

    2023.08.09 main 경로 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Routes,Route} from 'react-router-dom';
 import Login from './login/Login';
 import Sign from './sign/sign';
+import Main from './main/Main';
 
 function App() { 
     return(
         <div className="App">
             <Routes>
+                <Route path ="/" element = { <Main/>} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Sign />} />
             </Routes>
