ca6fb989c3856ccef029f97621c1c262b1ae2112
theeunseojo <ces80372@naver.com>
2023-08-12 21:47:18 +0900

    2023.08.12 App.js 미션페이지 경로 추가

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Login from './login/Login';
 import Sign from './sign/sign';
 import SimplePage from './simplepage/SimplePage';
 import Pluspage from './pluspage/pluspage';
+import MissionPage from './missonpage/MissionPage';
 
 function App() { 
     return(
         <div className="App">
             <Routes>
+                <Route path ="/missonpage" element = {<MissionPage/>}/>
                 <Route path ="/pluspage" element = { <Pluspage/>} />    
                 <Route path ="/simplepage" element = { <SimplePage/>} />
                 <Route path="/login" element={<Login />} />
